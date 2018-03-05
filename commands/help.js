6c6d49377a582ea08d7da5e72202814b8cb8764c
VorgSpring <drmrSP@yandex.ru>
2018-03-05 23:31:48 +0300

    created server

--- commands/help.js
+++ commands/help.js
@@ -4,12 +4,14 @@ const author = require(`./author`);
 const description = require(`./description`);
 const license = require(`./license`);
 const version = require(`./version`);
+const server = require(`./server`);
 
 const commands = [
   author,
   description,
   license,
-  version
+  version,
+  server
 ];
 
 const help = commands.map((command) => `${colors.gray(command.name)} — ${colors.green(command.description)}`).join(`\n\t`);
